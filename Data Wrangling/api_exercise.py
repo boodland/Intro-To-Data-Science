bad0ae1639aaad274e54af910269450593d5ef92
Alberto Arias <alberto@boodland.com>
2018-01-23 19:39:21 +0100

    Move current state of the course to github

---
+++ Data Wrangling/api_exercise.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+def api_get_request(url):
+    # In this exercise, you want to call the last.fm API to get a list of the
+    # top artists in Spain. The grader will supply the URL as an argument to
+    # the function; you do not need to construct the address or call this
+    # function in your grader submission.
+    # 
+    # Once you've done this, return the name of the number 1 top artist in
+    # Spain. 
+    
+    data = requests.get(url).text
+    json_data = json.loads(data)
+    top_artist = json_data['topartists']['artist']
+    return top_artist[0]['name']
